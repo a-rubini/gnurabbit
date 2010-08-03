023d5c6215fd6e5c4b544dbcb2bcafd66605866f
Alessandro Rubini <rubini@gnudd.com>
2010-08-03 08:56:43 +0200

    compat.h: added my trivial dumpstruct tool

--- kernel/compat.h
+++ kernel/compat.h
@@ -23,3 +23,20 @@ static inline void writeq(__u64 val, volatile void __iomem *addr)
 
    #endif
 #endif /* X86 */
+
+/* Hack... something I sometimes need */
+static void dumpstruct(char *name, void *ptr, int size)
+{
+	int i;
+	unsigned char *p = ptr;
+
+	printk("dump %s at %p (size 0x%x)\n", name, ptr, size);
+	for (i = 0; i < size; ) {
+		printk("%02x", p[i]);
+		i++;
+		printk(i & 3 ? " " : i & 0xf ? "  " : "\n");
+	}
+	if (i & 0xf)
+		printk("\n");
+}
+
